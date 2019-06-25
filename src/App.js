5bf062df016b2a9aa776a145647995ed08123a3f
Lavanya1510 <lavanya.dhasarathan@gmail.com>
2019-06-25 10:45:14 +0530

    forms

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Up from './components/Up';
+import Form from './components/Form';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+    <Up />
+    <Form />
+    </div>
+  );
+}
+
+export default App;
